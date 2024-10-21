15a3e7deec2ae7ed23016c1453ae919dfe98399a
Ariel Ebersberger <ariel@ebersberger.io>
2024-10-22 00:30:21 +0200

    Add tests for environment generation/sampling

--- src/modules/environment/gymnasium.py
+++ src/modules/environment/gymnasium.py
@@ -7,13 +7,15 @@ import minigrid
 import numpy as np
 import numpy.typing as npt
 import tqdm
+from enum import Enum
+from minigrid.wrappers import ImgObsWrapper
 
+from .minigrid_wrappers import FullyObsWrapper
 from src.framework.logging import Logger
 from src.framework.transforming import TransformationBlock
 from src.typing.pipeline_objects import XData
 
 logger = Logger()
-
 
 @dataclass
 class GymnasiumBuilder(TransformationBlock):
@@ -25,9 +27,18 @@ class GymnasiumBuilder(TransformationBlock):
     environment: str
 
     def __post_init__(self):
-        minigrid.register_minigrid_envs()
+        """Initialize the Gymnasium Environment."""
+        if "MiniGrid-BlockedUnlockPickup-v0" not in gym.envs.registry:
+            minigrid.register_minigrid_envs()
         self.env = gym.make(self.environment)
-        self.env.reset(seed=42)
+        
+        # Check if the environment is a minigrid environment
+        if not issubclass(type(self.env.unwrapped), minigrid.minigrid_env.MiniGridEnv):
+            raise ValueError("Currently only MiniGrid environments are supported.")
+        
+        self.env = FullyObsWrapper(self.env)    # Output fully observable grid
+        self.env = ImgObsWrapper(self.env)      # Output only numpy array
+        self.env.reset(seed=42)                 # Seed the environment
 
     def custom_transform(self, data: XData) -> npt.NDArray[np.float32]:
         """Apply a custom transformation to the data.
@@ -74,33 +85,32 @@ class GymnasiumSampler(TransformationBlock):
         logger.info("Sampling Environment")
 
         # Identify the important parameters
-        # num_actions = env.action_space.n
-        grid_size = env.unwrapped.grid.encode().shape
+        grid_size = (env.unwrapped.width, env.unwrapped.height)
 
         # Sample the Environment
-        x_states = np.empty((self.num_samples, *grid_size), dtype=np.int8)
-        x_actions = np.empty((self.num_samples, 1), dtype=np.int8)
-        y_states = np.empty((self.num_samples, *grid_size), dtype=np.int8)
-        y_rewards = np.empty((self.num_samples, 1), dtype=np.float32)
+        data.x_states = np.empty((self.num_samples, *grid_size, 3), dtype=np.int8)
+        data.x_actions = np.empty((self.num_samples, 1), dtype=np.int8)
+        data.y_states = np.empty((self.num_samples, *grid_size, 3), dtype=np.int8)
+        data.y_rewards = np.empty((self.num_samples, 1), dtype=np.float32)
         samples_collected = 0
 
         progress_bar = tqdm.tqdm(total=self.num_samples, desc="Sampling Environment", unit="samples")
         while samples_collected < self.num_samples:
-            _observation, _info = env.reset()
+            observation, _info = env.reset()
             for _ in range(self.num_samples_per_env):
                 # Random Action
                 action = env.action_space.sample()
 
                 # Save State and Action Chosen
-                x_states[samples_collected] = env.unwrapped.grid.encode()
-                x_actions[samples_collected] = action
+                data.x_states[samples_collected] = observation
+                data.x_actions[samples_collected] = action
 
                 # Take a step
-                _observation, reward, terminated, truncated, _info = env.step(action)
+                observation, reward, terminated, truncated, _info = env.step(action)
 
                 # Record the next state and reward
-                y_states[samples_collected] = env.unwrapped.grid.encode()
-                y_rewards[samples_collected] = reward
+                data.y_states[samples_collected] = observation
+                data.y_rewards[samples_collected] = reward
                 samples_collected += 1
 
                 # Misc
@@ -112,17 +122,11 @@ class GymnasiumSampler(TransformationBlock):
 
         env.close()
 
-        # Store the data in the XData object
-        data.x_states = x_states
-        data.x_actions = x_actions
-        data.y_states = y_states
-        data.y_rewards = y_rewards
-
         # Split the data into training and testing data
         # TODO: Take into consideration 'reserved' states, which are states
         # that are not allowed to be in the training data. These
         # should only be in the testing data.
-        data.train_indices = np.random.choice(len(x_states), int(self.perc_train * len(x_states)), replace=False)
-        data.validation_indices = np.setdiff1d(np.arange(len(x_states)), data.train_indices)
+        data.train_indices = np.random.choice(self.num_samples, int(self.perc_train * self.num_samples), replace=False)
+        data.validation_indices = np.setdiff1d(np.arange(self.num_samples), data.train_indices)
 
         return data
---
+++ tests/modules/environment/test_gymnasium.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from src.modules.environment.gymnasium import GymnasiumBuilder, GymnasiumSampler
+from src.typing.pipeline_objects import XData
+
+
+def test_gymnasium_builder():
+    """Test the GymnasiumBuilder class."""
+    builder = GymnasiumBuilder(environment="MiniGrid-Empty-5x5-v0")
+    data = builder.transform(XData())
+    assert data.env is not None
+    
+def test_gymnsasium_sampler():
+    """Test the GymnasiumSampler class."""
+    builder = GymnasiumBuilder(environment="MiniGrid-Empty-5x5-v0")
+    data = builder.transform(XData())
+    
+    num_samples = 10
+    num_samples_per_env = 5
+    
+    sampler = GymnasiumSampler(num_samples=num_samples, num_samples_per_env=num_samples_per_env, perc_train=0.8)
+    data = sampler.transform(data)
+    
+    # Check all arrays have the correct length
+    assert len(data.x_states) == len(data.y_states) == len(data.x_actions) == len(data.y_rewards) == num_samples
+    
+    # Check shape of states
+    size = (data.env.unwrapped.width, data.env.unwrapped.height, 3)
+    assert data.x_states.shape[1:] == data.y_states.shape[1:] == size
+    
+    # Check shape of rewards and actions
+    assert data.x_actions.shape[1:] == data.y_rewards.shape[1:] == (1,)
+    
+    # Check that exactly one agent is present on the grid
+    target = [10, 0]    # Agent Encoding. Last position indicates direction
+    mask = np.ones((num_samples, *size[:-1]), dtype=bool)
+    for idx in [0, 1]:
+        mask &= (data.x_states[..., idx] == target[idx])
+    assert np.all(np.sum(mask, axis=(1, 2)))
+    
+    # Check that the direction of agent is between 0 and 4
+    directions = data.x_states[mask][:, -1]
+    assert np.all(directions >= 0)
+    assert np.all(directions < 4)
