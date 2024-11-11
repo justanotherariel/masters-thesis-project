a25b04c10083d4c6719f1e3880e01dd9e907dca4
Ariel Ebersberger <ariel@ebersberger.io>
2024-11-11 12:25:36 +0100

    Add manual.py to test environments

---
+++ manual.py
@@ -0,0 +1,18 @@
+from minigrid.core.world_object import Goal, Wall
+from minigrid.manual_control import ManualControl
+import minigrid
+import gymnasium as gym
+import pygame
+
+env_name = "MiniGrid-KeyCorridorS6R3-v0"
+
+# Initialize Pygame
+pygame.init()
+
+# Create the environment
+minigrid.register_minigrid_envs()
+env = gym.make(env_name, render_mode="human")
+
+# Start manual control
+manual_control = ManualControl(env.unwrapped)
+manual_control.start()
