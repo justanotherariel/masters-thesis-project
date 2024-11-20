e8714e0cff6f60148e12c914bd00b04a74e6eb5d
Ariel Ebersberger <ariel@ebersberger.io>
2024-11-20 09:59:36 +0100

    Add scorer

--- src/modules/scoring/minigrid_scorer.py
+++ src/modules/scoring/minigrid_scorer.py
@@ -2,7 +2,10 @@ from typing import Any
 
 from src.framework.transforming import TransformationBlock
 from src.typing.pipeline_objects import XData
+import torch
+from src.framework.logging import Logger
 
+logger = Logger()
 
 class MinigridScorer(TransformationBlock):
     """Score the predictions of the model."""
@@ -23,15 +26,55 @@ class MinigridScorer(TransformationBlock):
         :param kwargs: Any additional arguments
         :return: The transformed data
         """
-
-        # Check tokentype accuracy
-
-        # Check Observation FieldType accuaracy
+        
+        ti = self.info['token_index']
+        ti.discrete = True  # ToDo: fetch from info, supplied by model setup
+        
+        obs_pred, reward_pred = data.predictions
+        obs_target, reward_target = data.targets
+        
+        # Check Observation object accuaracy
+        obs_pred_obj = obs_pred[..., ti.observation[0]].argmax(axis=-1)
+        obs_target_obj = obs_target[..., ti.observation[0]].argmax(axis=-1)
+        obs_obj_accuracy = (obs_pred_obj == obs_target_obj).sum()/obs_pred_obj.numel()
+        
+        obs_obj_accuracy_no_walls = (obs_pred_obj[:, 1:-1, 1:-1] == obs_target_obj[:, 1:-1, 1:-1]).sum()/obs_pred_obj.numel()
 
         # Check Observation color accuracy
+        obs_pred_color = obs_pred[..., ti.observation[1]].argmax(axis=-1)
+        obs_target_color = obs_target[..., ti.observation[1]].argmax(axis=-1)
+        obs_color_accuracy = (obs_pred_color == obs_target_color).sum()/obs_pred_color.numel()
 
         # Check Observation state accuracy
+        obs_pred_state = obs_pred[..., ti.observation[2]].argmax(axis=-1)
+        obs_target_state = obs_target[..., ti.observation[2]].argmax(axis=-1)
+        obs_state_accuracy = (obs_pred_state == obs_target_state).sum()/obs_pred_state.numel()
+
+        # Check Observation agent accuracy
+        obs_pred_agent = obs_pred[..., ti.observation[3]].argmax(axis=-1)
+        obs_target_agent = obs_target[..., ti.observation[3]].argmax(axis=-1)
+        obs_agent_accuracy = (obs_pred_agent == obs_target_agent).sum()/obs_pred_agent.numel()
 
         # Check Reward accuracy
+        reward_accuracy = torch.isclose(reward_target, reward_pred, atol=0.1).sum()/len(reward_target)
+        
+        # Log the results
+        logger.info(f"Observation Object Accuracy: {obs_obj_accuracy}")
+        logger.info(f"Observation Object Accuracy (no walls): {obs_obj_accuracy_no_walls}")
+        logger.info(f"Observation Color Accuracy: {obs_color_accuracy}")
+        logger.info(f"Observation State Accuracy: {obs_state_accuracy}")
+        logger.info(f"Observation Agent Accuracy: {obs_agent_accuracy}")
+        logger.info(f"Reward Accuracy: {reward_accuracy}")
+        
+        logger.log_to_external(
+            message={
+                "Observation Object Accuracy": obs_obj_accuracy,
+                "Observation Object Accuracy (no walls)": obs_obj_accuracy_no_walls,
+                "Observation Color Accuracy": obs_color_accuracy,
+                "Observation State Accuracy": obs_state_accuracy,
+                "Observation Agent Accuracy": obs_agent_accuracy,
+                "Reward Accuracy": reward_accuracy,
+            },
+        )
 
         return data
