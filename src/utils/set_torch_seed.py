9f1edabaf03ae9698e53dd0fe9afa22b4e44a18d
Ariel Ebersberger <ariel@ebersberger.io>
2025-02-16 13:25:21 +0100

    Fix reproducibility

--- src/utils/set_torch_seed.py
+++ src/utils/set_torch_seed.py
@@ -3,22 +3,36 @@
 import torch
 
 from src.framework.logging import Logger
+import random
+import numpy as np
 
 logger = Logger()
 
 
-def set_torch_seed(seed: int = 42) -> None:
+def set_torch_seed(seed: int = 42, deterministic=True) -> None:
     """Set torch seed for reproducibility.
 
     :param seed: seed to set
 
     :return: None
     """
+    # Set Python's random seed
+    random.seed(seed)
+    
+    # Set NumPy's random seed
+    np.random.seed(seed)
+    
+    # Set PyTorch's random seed
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-    # When running on the CuDNN backend, two further options must be set
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    logger.info(f"Seed set to {seed}")
+    
+    # Set CUDA's random seed if available
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # for multi-GPU
+    
+    # Make CUDA operations deterministic
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    torch.set_deterministic_debug_mode(1 if deterministic else 0)
+    # torch.use_deterministic_algorithms(deterministic)
